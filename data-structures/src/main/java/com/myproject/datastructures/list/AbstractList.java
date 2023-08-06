c6c8158dd3d994c682faaebff76f1110fb2e2e40
SerJey <haccent12@gmail.com>
2023-08-06 17:06:45 +0200

    made Absract List

--- data-structures/src/main/java/com/myproject/datastructures/list/AbstractList.java
+++ data-structures/src/main/java/com/myproject/datastructures/list/AbstractList.java
@@ -1,5 +1,16 @@
 package com.myproject.datastructures.list;
 
 public abstract class AbstractList<T> implements List<T> {
-
+    public abstract void add(T value);
+    public abstract void add(T value, int index);
+    public abstract T remove(int index);
+    public abstract T get(int index);
+    public abstract T set(T value, int index);
+    public abstract void clear();
+    public abstract int size();
+    public abstract boolean isEmpty();
+    public abstract boolean contains(T value);
+    public abstract int indexOf(T value);
+    public abstract int lastIndexOf(T value);
+    public abstract String toString();
 }
